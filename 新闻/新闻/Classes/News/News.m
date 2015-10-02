13955efdcf2d8672ebc229238b834c31b8c8c8d9
sharui <sr_sharui@qq.com>
2015-10-02 15:07:53 +0800

    字典转模型

--- 新闻/新闻/Classes/News/News.m
+++ 新闻/新闻/Classes/News/News.m
@@ -13,16 +13,38 @@
 + (instancetype)newsWithDict:(NSDictionary *)dict {
     id obj = [[self alloc] init];
     
-    [obj setValuesForKeysWithDictionary:dict];
+    NSArray *propert = @[@"title",@"digest",@"relpyCount",@"imgsrc"];
+    for (NSString *key in propert) {
+        if (dict [key] !=nil) {
+            [obj setValue:dict[key] forKey:key];
+        }
+    }
     
     return obj;
 }
 
 + (void)loadNewsListWithURLString:(NSString *)urlString {
-    [[NetworkTools shareNetworkTools] GET:urlString parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
-        NSLog(@"%@",responseObject);
+    [[NetworkTools shareNetworkTools] GET:urlString parameters:nil success:^(NSURLSessionDataTask *task, NSDictionary *responseObject) {
+        //URL不同，第一层字典的key 不同
+        //利用keyEnumerator.nextObjict 获取字典第一个key
+        //主要用来遍历字典
+        
+        NSLog(@"%@",responseObject.keyEnumerator.nextObject);
+        //根据key 拿到第一个数组
+        NSArray *array = responseObject[responseObject.keyEnumerator.nextObject];
+        NSMutableArray *arrayM = [NSMutableArray arrayWithCapacity:array.count];
+        for (NSDictionary *dict in array) {
+            [arrayM addObject:[self newsWithDict:dict]];
+        }
+        NSLog(@"%@",array);
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         NSLog(@"%@",error);
     }];
+}
+
+- (NSString *)description {
+    NSArray *propert = @[@"title",@"digest",@"replyCount",@"imgsrc"];
+    NSDictionary *dict = [self dictionaryWithValuesForKeys:propert];
+    return [NSString stringWithFormat:@"%@",dict];
 }
 @end
