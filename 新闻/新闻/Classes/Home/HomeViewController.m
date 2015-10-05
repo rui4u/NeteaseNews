0556d76b0aadc1706414d96023de1c55b8ca77b3
sharui <sr_sharui@qq.com>
2015-10-05 17:44:08 +0800

    界面布局

--- 新闻/新闻/Classes/Home/HomeViewController.m
+++ 新闻/新闻/Classes/Home/HomeViewController.m
@@ -9,7 +9,9 @@
 #import "HomeViewController.h"
 #import "Channel.h"
 #import "ChannelLabel.h"
-@interface HomeViewController ()
+@interface HomeViewController ()<UICollectionViewDelegate,UICollectionViewDataSource>
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) IBOutlet UICollectionViewFlowLayout *layout;
 
 //频道数据
 @property (nonatomic, strong) NSArray *channelList;
@@ -26,6 +28,17 @@
     [self setupChannel];
 }
 
+- (void)viewDidLayoutSubviews {
+    self.layout.itemSize = self.collectionView.bounds.size;
+    self.layout.minimumInteritemSpacing = 0;
+    self.layout.minimumLineSpacing = 0;
+    self.layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    
+    self.collectionView.showsHorizontalScrollIndicator = NO;
+    self.collectionView.pagingEnabled = YES;
+    
+
+}
 - (void)setupChannel {
     
     //取消scrollView的缩进
@@ -40,6 +53,19 @@
         [self.channelView addSubview:l];
     }
     self.channelView.contentSize = CGSizeMake(x+margin, h);
+}
+
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+    return self.channelList.count;
+}
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell * cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"CollectionCell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor colorWithRed:((float)arc4random_uniform(256) / 255.0) green:((float)arc4random_uniform(256) / 255.0) blue:((float)arc4random_uniform(256) / 255.0) alpha:1.0];
+    return cell;
 }
 #pragma mark - 懒加载
 - (NSArray *)channelList {
